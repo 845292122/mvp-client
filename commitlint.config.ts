0fb45d04fc8e25f975570f7dd2d6fafa9fe53f5e
Edison.Cheung <845292122@qq.com>
2025-10-04 11:08:33 +0800

    chore: 整合husky commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,12 @@
+/**
+ * feat: 添加新功能
+ * fix: 修复 bug
+ * docs: 文档更新
+ * style: 代码风格调整（空格/缩进等）
+ * refactor: 代码重构
+ * test: 测试相关
+ * chore: 构建/工具变更
+ */
+export default {
+  extends: ['@commitlint/config-conventional']
+}
